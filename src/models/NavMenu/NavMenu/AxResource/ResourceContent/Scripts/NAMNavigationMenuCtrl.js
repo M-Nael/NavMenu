903d847a9eb924e42601c2a20d2bfb1f57a2826f
Mohammed Nael Mahmoud <49475137+M-Nael@users.noreply.github.com>
2025-02-02 08:33:59 +0000

    Commit 2

--- src/models/NavMenu/NavMenu/AxResource/ResourceContent/Scripts/NAMNavigationMenuCtrl.js
+++ src/models/NavMenu/NavMenu/AxResource/ResourceContent/Scripts/NAMNavigationMenuCtrl.js
@@ -5,6 +5,14 @@
         var self = this;
         $dyn.ui.Control.apply(self, arguments);
         $dyn.ui.applyDefaults(self, data, $dyn.ui.defaults.NAMNavigationMenuCtrl);
+        $(element).css("background-color", "yellow");
+        var m_bSendCommandToJavaScript = false;
+        $dyn.observe(this.SendCommandToJavaScript, function (_dtParam) {
+            if (m_bSendCommandToJavaScript == true) {
+                alert("SendCommandToJavaScript Invoked");
+            }
+            m_bSendCommandToJavaScript = true;
+        });
     }
     $dyn.controls.NAMNavigationMenuCtrl.prototype = $dyn.extendPrototype($dyn.ui.Control.prototype, {});
 })();
